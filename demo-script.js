e721c2642952e45fb6987bdc5696dbecbe0878df
katokdoescode <snakedoc.md@gmail.com>
2023-06-12 10:45:07 +0400

    Fix text length bug

--- demo-script.js
+++ demo-script.js
@@ -6,10 +6,10 @@ article.oninput = () => {
 	const isFilled = article.classList.contains('filled');
 	const isOverfilled = article.classList.contains('overfilled');
 
-	if(article.innerText.length === count) article.classList.add('filled');
+	if(article.textContent.length === count) article.classList.add('filled');
 	else if (isFilled) article.classList.remove('filled');
 
-	if(article.innerText.length > count) article.classList.add('overfilled');
+	if(article.textContent.length > count) article.classList.add('overfilled');
 	else if (isOverfilled) article.classList.remove('overfilled');
 };
 
