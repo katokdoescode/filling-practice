79c689b92a3ae12a5c86700479540bdf8269c40c
katokdoescode <snakedoc.md@gmail.com>
2023-06-12 10:30:36 +0400

    Init practice

---
+++ demo-script.js
@@ -0,0 +1,32 @@
+const article = document.querySelector('article');
+window.count = 4992;
+
+article.oninput = () => {
+	const count = window.count;
+	console.debug(count)
+	const isFilled = article.classList.contains('filled');
+	const isOverfilled = article.classList.contains('overfilled');
+
+	if(article.innerText.length === count) article.classList.add('filled');
+	else if (isFilled) article.classList.remove('filled');
+
+	if(article.innerText.length > count) article.classList.add('overfilled');
+	else if (isOverfilled) article.classList.remove('overfilled');
+};
+
+const handleSubmit = (event) => {
+	event.preventDefault();
+
+	const formData = new FormData(event.target);
+	const objectData = Object.fromEntries(formData);
+	const { letters } = objectData;
+	window.count = Number(letters);
+
+	const savedMessage = document.getElementById('saved');
+	showMessage(savedMessage, 2000);
+}
+
+const showMessage = (target, timeout) => {
+	target.hidden = false;
+	setTimeout(() => (target.hidden = true), timeout)
+}
---
+++ script.js
@@ -0,0 +1,12 @@
+const article = document.querySelector('article');
+const COUNT = 4992;
+article.oninput = () => {
+	const isFilled = article.classList.contains('filled');
+	const isOverfilled = article.classList.contains('overfilled');
+
+	if(article.innerText.length === COUNT) article.classList.add('filled');
+	else if (isFilled) article.classList.remove('filled');
+
+	if(article.innerText.length > COUNT) article.classList.add('overfilled');
+	else if (isOverfilled) article.classList.remove('overfilled');
+};
